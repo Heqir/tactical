cef4ab259c81f5909d1ff1e16f001ee5a893fa35
Jakob Svensson <jakob-_-92@hotmail.com>
2013-11-18 18:49:34 +0100

    Added a test class for Tactical

---
+++ tactical-main/test/se/tactical/TacticalTest.java
@@ -0,0 +1,14 @@
+package se.tactical;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TacticalTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
