421883356008f8036420d23fecbc64f4811fc9e9
Jakob Svensson <jakob-_-92@hotmail.com>
2013-11-18 18:50:05 +0100

    Tactical now creates a gamescreen and camera

--- tactical-main/src/se/tactical/Tactical.java
+++ tactical-main/src/se/tactical/Tactical.java
@@ -1,13 +1,28 @@
 package se.tactical;
+import se.tactical.screens.GameScreen;
+
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 
 public class Tactical extends Game{
-
+	
+	private OrthographicCamera camera;
+	private SpriteBatch batch;
+	private GameScreen gameScreen;
+	
 	@Override
 	public void create() {
-		// TODO Auto-generated method stub
 		
+		float w = Gdx.graphics.getWidth();
+		float h = Gdx.graphics.getHeight();
+		
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, w, h);
+		batch = new SpriteBatch();		
+		gameScreen = new GameScreen(); 
 	}
 
 }
