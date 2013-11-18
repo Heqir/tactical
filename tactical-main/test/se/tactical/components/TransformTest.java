567a00b42e20451f02f9cfbcf697720c77eb801b
Jakob Svensson <jakob-_-92@hotmail.com>
2013-11-18 17:25:37 +0100

    Added test structure and some test cases

---
+++ tactical-main/test/se/tactical/components/TransformTest.java
@@ -0,0 +1,14 @@
+package se.tactical.components;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TransformTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ tactical-main/test/se/tactical/systems/RenderSystemTest.java
@@ -0,0 +1,14 @@
+package se.tactical.systems;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class RenderSystemTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
