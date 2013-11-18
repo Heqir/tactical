e5a7173b83a888566684e5a4e0f354e7ad4ef617
Heqir <markus_xtrom@hotmail.com>
2013-11-18 19:35:55 +0100

    Added test class for InputController

---
+++ tactical-main/test/se/tactical/input/InputControllerTest.java
@@ -0,0 +1,14 @@
+package se.tactical.input;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+public class InputControllerTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
